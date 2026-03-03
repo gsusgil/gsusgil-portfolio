0eb8261f1c0974146db23606afa04d6a9a78b04f
gsusgil <gsusgildev@gmail.com>
2026-03-03 01:42:09 +0100

    fix: bundle scripts for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// astro.config.mjs
 // @ts-check
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  output: "static",       // ✅ Sitio estático (cero serverless functions)
+  output: "static",
   adapter: vercel(),
 });
